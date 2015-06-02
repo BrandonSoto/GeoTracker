489fe2d9bd83c57dd81da3ca9493032754f3712e
loralyn <loralyn@uw.edu>
2015-06-01 21:24:57 -0700

    LoginActivityTest complete

--- app/src/androidTest/java/edu/uw/ProjectMayhem/controllers/RegistrationActivityTest.java
+++ app/src/androidTest/java/edu/uw/ProjectMayhem/controllers/RegistrationActivityTest.java
@@ -11,23 +11,19 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.robotium.solo.Solo;
 
-/** This test class tests all pre and post variants of the LoginActivity class as well as the buttons. */
-public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<RegistrationActivity> {
     private Solo solo;
 
-    /** constructs the LoginActivity */
-    public LoginActivityTest() {
-        super(LoginActivity.class);
+    public RegistrationActivityTest() {
+        super(RegistrationActivity.class);
     }
 
-    /** sets up conditions prior to each test. */
     @Override
     public void setUp() throws Exception {
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
-    /** resets conditions following each test. */
     @Override
     public void tearDown() throws Exception {
         //tearDown() is run after a test case has finished.
@@ -35,7 +31,6 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         solo.finishOpenedActivities();
     }
 
-    /** Tests to make sure email and password is set and longer than 1 character each. */
     public void testARequiredFields() {
         int i;
         solo.unlockScreen();
@@ -71,13 +66,26 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         }
     }
 
-    /** Tests registration button. */
-    public void testBRegisterButton() {
+    public void testBOrientation() {
         int i;
         solo.clickOnButton("Register");
+        solo.enterText(0, "test@dummy.com");
+        solo.enterText(1, "password");
+        solo.enterText(2, "password");
+        solo.enterText(3, "security answer");
 
-        boolean textFound = solo.searchText("Registration");
-        assertTrue("Register button failed", textFound);
+        solo.setActivityOrientation(Solo.LANDSCAPE);
+        boolean textFound = solo.searchText("test@dummy.com");
+        assertTrue("Orientation change failed", textFound);
+
+        //for a pause
+        for (i = 0; i < 10000; i++) {
+
+        }
+
+        solo.setActivityOrientation(Solo.PORTRAIT);
+        textFound = solo.searchText("test@dummy.com");
+        assertTrue("Orientation change failed", textFound);
 
         //for a pause
         for (i = 0; i < 10000; i++) {
@@ -85,39 +93,18 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         }
     }
 
-    /** Tests Login button. */
     public void testCLogin() {
         int i;
         solo.enterText(0, "loralyn@uw.edu");
         solo.enterText(1, "password");
         solo.clickOnButton("Login");
-        //for a pause
-        for (i = 0; i < 10000; i++) {
-
-        }
         boolean textFound = solo.searchText("loralyn@uw.edu has signed in!");
         assertTrue("Login failed", textFound);
 
-    }
-
-    /** Tests reset password button. */
-    public void testDResetPasswordButton() {
-        int i;
-        solo.clickOnButton("Forgot Password");
-
-        boolean textFound = solo.searchText("Reset Password");
-        assertTrue("Reset password button failed", textFound);
-
-        /**
-        solo.enterText(0, "loralyn@uw.edu");
-        solo.clickOnButton("Reset Password");
         //for a pause
         for (i = 0; i < 10000; i++) {
 
         }
-        boolean textFound = solo.searchText("An email with instructions");
-        assertTrue("Reset password failed", textFound);
-         */
     }
 
 }
